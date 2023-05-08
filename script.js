3cfa247728635b619b6b25e2011d9764028048f6
adlani10 <94886532+adlani10@users.noreply.github.com>
2023-05-08 08:20:40 -0700

    Initial commit.

---
+++ script.js
@@ -0,0 +1,61 @@
+var timeLeft = 5;
+var number = getRandomNumber();
+var color = getRandomColor();
+var lastNumber = -1;
+var lastColor = '';
+
+function getRandomNumber() {
+  var min = 1;
+  var max = parseInt(document.getElementById("number-range-slider").value);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomColor() {
+  var colors = ['green', 'red', 'blue', 'orange'];
+  var color = colors[Math.floor(Math.random() * colors.length)];
+  while (color === lastColor) {
+    color = colors[Math.floor(Math.random() * colors.length)];
+  }
+  lastColor = color;
+  return color;
+}
+
+function displayNumberAndColor() {
+  number = getRandomNumber();
+  color = getRandomColor();
+  while (number === lastNumber && color === lastColor) {
+    number = getRandomNumber();
+    color = getRandomColor();
+  }
+  lastNumber = number;
+  document.body.className = color;
+  document.getElementById('output').innerHTML = number;
+}
+
+function countdown() {
+  if (timeLeft == 0) {
+    timeLeft = parseInt(document.getElementById("interval-slider").value);
+    displayNumberAndColor();
+  } else {
+    document.getElementById("countdown").innerHTML = timeLeft + " seconds";
+    timeLeft--;
+  }
+}
+
+
+displayNumberAndColor();
+setInterval(countdown, 1000);
+
+const timeSlider = document.getElementById('time-slider');
+const timeLabel = document.getElementById('time-label');
+timeSlider.addEventListener('input', function() {
+  const value = this.value;
+  timeLabel.innerText = `Interval: ${value} seconds`;
+});
+
+const numSlider = document.getElementById('num-slider');
+const numLabel = document.getElementById('num-label');
+numSlider.addEventListener('input', function() {
+  const value = this.value;
+  numLabel.innerText = `Number Range: 1 - ${value}`;
+});
